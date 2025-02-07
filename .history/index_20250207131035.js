a751e5594f135e0325fdbeb38630501f46008e0d
Brandon Taylor <brandon@thecreativekingdomco.com>
2025-02-07 17:06:31 -0600

    Fix Responsiveness, Custom Input feature

---
+++ .history/index_20250207131035.js
@@ -0,0 +1,159 @@
+const billInput = document.querySelector("#bill-input");
+const peopleInput = document.querySelector("#number-of-people-input");
+const tip = document.querySelector("#tip-amount");
+const total = document.querySelector("#total-amount");
+const tipButtons = document.querySelectorAll('.tip-button')
+
+const calculateBtn = document.querySelector("#calculate-button");
+const resetBtn = document.querySelector("#reset-button");
+const formError = document.querySelector('#form-error')
+const peopleError = document.querySelector("#people-error");
+const billError = document.querySelector('#bill-error')
+const tipError = document.querySelector('#tip-error')
+
+// let billAmount
+// let tipPercentage
+// let numberOfPeople
+let tipAmount
+let totalBill
+
+tipButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.classList.contains('active')) {
+            button.classList.remove('active')
+        } else {
+            button.classList.add('active')
+        }
+        // }
+        // tipPercentage = button.value
+        // console.log(tipPercentage)
+        // return tipPercentage
+    })
+})
+
+function validateAmount(input) {
+    if(!isNaN(input) && input !== "" && input > 0) {
+        const dollarAmount = parseFloat(input);
+        return dollarAmount
+
+        // billAmount = dollarAmount;
+        // console.log(billAmount);
+    } else {
+        console.log("Invalid input. Please enter a valid number");
+        billError.textContent = "Please enter a valid number";
+    }
+}
+
+function validatePeople(input) {
+    if(!isNaN(input) && input !== "" && input > 0) {
+        const people = parseFloat(input);
+        return people
+        
+        // numberOfPeople = people;
+        // console.log(numberOfPeople)
+    } else {
+        console.log("Invalid input. Please enter a valid number");
+        peopleError.textContent = "Please enter a valid number";
+    }
+}
+
+function calculateTipAmount(bill, tipCalculated, people) {
+    const calculatedTip = (bill * tipCalculated) / people
+    // console.log(calculatedTip)
+    // tipAmount = calculatedTip
+    tip.innerText = `$${calculatedTip.toFixed(2)}`
+}
+
+function calculateTotalAmount(bill, tipAmount, people) {
+    const totalBill = (bill + tipAmount) / people
+    // console.log(totalBill); 
+    // billAmount = totalBill
+    total.innerText = `$${totalBill.toFixed(2)}`
+}
+
+calculateBtn.addEventListener("click", () => {    
+    let hasError = false;
+    const billAmount = document.querySelector("#bill-input").value;
+    console.log(billAmount)
+    const tipPercentage = document.querySelector(".active").value;
+    console.log(tipPercentage)
+    const numberOfPeople = document.querySelector("#number-of-people-input").value;
+    console.log(numberOfPeople)
+
+    
+
+    
+    if (billAmount === undefined || billAmount === "") {
+        billError.innerText = "Please enter a bill amount";
+        hasError = true;
+        tip.innerText = "$0.00";
+        total.innerText = "$0.00";
+    } else {
+        billError.innerText = "";
+    }
+    
+    if (tipPercentage === undefined || tipPercentage === "") {
+        tipError.innerText = "Please select a tip percentage";
+        hasError = true;
+        tip.innerText = "$0.00";
+        total.innerText = "$0.00";
+    } else {
+        tipError.innerText = "";
+    }
+    
+    if (numberOfPeople === undefined || numberOfPeople === "") {
+        peopleError.innerText = "Please enter a valid number";
+        hasError = true;
+        tip.innerText = "$0.00";
+        total.innerText = "$0.00";
+    } else {
+        peopleError.innerText = "";
+    }
+    
+    if(hasError) {
+        formError.innerText = "Please fill out all fields"
+    } else {
+        formError.innerText = ""
+    }
+    
+    calculateTipAmount(billAmount, tipPercentage, numberOfPeople)
+    calculateTotalAmount(billAmount, tipAmount, numberOfPeople)
+
+    calculateBtn.disabled = true;
+
+})
+
+resetBtn.addEventListener("click", () => {
+    billInput.value = ""
+    peopleInput.value = ""
+    tipAmount = 0
+    totalBill = 0
+    tip.innerText = "$0.00"
+    total.innerText = "$0.00"
+    tipButtons.forEach(button => {
+        button.classList.remove('active')
+    })
+    tipPercentage = 0
+    billAmount = 0
+    numberOfPeople = 0
+    formError.innerText = ""
+    billError.innerText = ""
+    tipError.innerText = ""
+    peopleError.innerText = ""
+
+    calculateBtn.disabled = false
+
+})
+
+billInput.addEventListener("input", () => {
+    console.log(billInput.value); 
+    validateAmount(billInput.value); 
+})
+
+peopleInput.addEventListener("input", () => {
+    console.log(peopleInput.value);
+    validatePeople(peopleInput.value);
+})
+
+
+
---
+++ .history/index_20250207135024.js
@@ -0,0 +1,139 @@
+const billInput = document.querySelector("#bill-input");
+const peopleInput = document.querySelector("#number-of-people-input");
+const tipAmountElement = document.getElementById("tip-amount");
+const totalAmountElement = document.getElementById("total-amount");
+const tipButtons = document.querySelectorAll('.tip-button')
+
+const calculateBtn = document.querySelector("#calculate-button");
+const resetBtn = document.querySelector("#reset-button");
+const formError = document.querySelector('#form-error')
+const peopleError = document.querySelector("#people-error");
+const billError = document.querySelector('#bill-error')
+const tipError = document.querySelector('#tip-error')
+
+let billAmount = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+tipButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        if(button.classList.contains('active')) {
+            button.classList.remove('active')
+        } else {
+            button.classList.add('active')
+        }
+        tipPercentage = button.value
+        console.log(tipPercentage)
+    })
+})
+
+function validateAmount(input) {
+    if(!isNaN(input) && input !== "" && input > 0) {
+        const dollarAmount = parseFloat(input);
+        billAmount = dollarAmount;
+    } else {
+        console.log("Invalid input. Please enter a valid number");
+        billError.textContent = "Please enter a valid number";
+    }
+}
+
+function validatePeople(input) {
+    if(!isNaN(input) && input !== "" && input > 0) {
+        const people = parseFloat(input);
+        numberOfPeople = people;
+        console.log(numberOfPeople)
+    } else {
+        console.log("Invalid input. Please enter a valid number");
+        peopleError.textContent = "Please enter a valid number";
+    }
+}
+
+function calculateTipAmount(bill, tipCalculated, people) {
+    return (bill * tipCalculated) / people
+}
+
+function calculateTotalAmount(bill, tipAmount, people) {
+    return (bill + tipAmount) / people
+    
+}
+
+calculateBtn.addEventListener("click", () => {    
+    let hasError = false;
+
+    if(billAmount === undefined || tipPercentage === undefined || numberOfPeople === undefined) {
+        formError.innerText = "Please fill out all fields";
+        hasError = true;
+    }
+    
+    if (billAmount === undefined || billAmount === "") {
+        billError.innerText = "Please enter a bill amount";
+        hasError = true;
+        tip.innerText = "$0.00";
+        total.innerText = "$0.00";
+    } else {
+        billError.innerText = "";
+    }
+    
+    if (tipPercentage === undefined || tipPercentage === "") {
+        tipError.innerText = "Please select a tip percentage";
+        hasError = true;
+        tip.innerText = "$0.00";
+        total.innerText = "$0.00";
+    } else {
+        tipError.innerText = "";
+    }
+    
+    if (numberOfPeople === undefined || numberOfPeople === "") {
+        peopleError.innerText = "Please enter a valid number";
+        hasError = true;
+        tip.innerText = "$0.00";
+        total.innerText = "$0.00";
+    } else {
+        peopleError.innerText = "";
+    }
+    
+    
+    calculateTipAmount(billAmount, tipPercentage, numberOfPeople)
+    calculateTotalAmount(billAmount, tipAmount, numberOfPeople)
+
+    tipAmountElement.innerText = `$${tipAmount.toFixed(2)}`;
+    totalAmountElement.innerText = `$${totalAmount.toFixed(2)}`;
+
+    calculateBtn.disabled = true;
+
+})
+
+resetBtn.addEventListener("click", () => {
+    billInput.value = ""
+    peopleInput.value = ""
+    tipAmount = 0
+    totalBill = 0
+    tip.innerText = "$0.00"
+    total.innerText = "$0.00"
+    tipButtons.forEach(button => {
+        button.classList.remove('active')
+    })
+    tipPercentage = 0
+    billAmount = 0
+    numberOfPeople = 0
+    formError.innerText = ""
+    billError.innerText = ""
+    tipError.innerText = ""
+    peopleError.innerText = ""
+
+    calculateBtn.disabled = false
+
+})
+
+// billInput.addEventListener("input", () => {
+//     console.log(billInput.value); 
+//     validateAmount(billInput.value); 
+// })
+
+// peopleInput.addEventListener("input", () => {
+//     console.log(peopleInput.value);
+//     validatePeople(peopleInput.value);
+// })
+
+
+
